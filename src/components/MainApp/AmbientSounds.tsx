e3af99adae713d6a37f8c128e0fcba683199a631
Marmo77 <matyskamela@gmail.com>
2025-12-05 18:27:48 +0100

    selected + aplications

--- src/components/MainApp/AmbientSounds.tsx
+++ src/components/MainApp/AmbientSounds.tsx
@@ -1,33 +1,34 @@
-import { CloudRainWind, Trees } from "lucide-react";
+import { CloudRainWind, Coffee, Moon, Trees, Waves } from "lucide-react";
 import React, { useState } from "react";
 import { Button } from "../ui/button";
+import { motion } from "motion/react";
+
+type Environment = "rain" | "forest" | "night" | "cafe" | "ocean" | null;
 
 type AmbientSoundsElementsProps = {
-  id: string;
+  id: Environment;
   icon: React.ElementType;
   label: string;
   color: string;
 };
-
-type Environment = "rain" | "forest" | "night" | "cafe" | "ocean" | null;
-
 function AmbientSounds() {
-  const [selectedSound, setSelectedSound] = useState<Environment>("rain");
+  const [selectedSound, setSelectedSound] = useState<Environment>(null);
 
   const AmbietSoundsElements: AmbientSoundsElementsProps[] = [
-    {
-      id: "rain",
-      icon: CloudRainWind,
-      label: "Rain",
-      color: "#7dd3fc",
-    },
-    {
-      id: "forest",
-      icon: Trees,
-      label: "Forest",
-      color: "#4ade80",
-    },
+    { id: "rain", icon: CloudRainWind, label: "Rain", color: "#7dd3fc" },
+    { id: "forest", icon: Trees, label: "Forest", color: "#4ade80" },
+    { id: "night", icon: Moon, label: "Night", color: "#a78bfa" },
+    { id: "cafe", icon: Coffee, label: "Café", color: "#fb923c" },
+    { id: "ocean", icon: Waves, label: "Ocean", color: "#38bdf8" },
   ];
+
+  const ToggleEnvironment = (elem: Environment) => {
+    if (selectedSound === elem) {
+      setSelectedSound(null);
+    } else {
+      setSelectedSound(elem);
+    }
+  };
 
   return (
     <div className="flex flex-col md:fixed bottom-0 left-0 z-10 py-2 mt-12 w-full bg-card/80 border-border/50 border-t border-x-0 border-b-0">
@@ -39,19 +40,21 @@ function AmbientSounds() {
               const Icon = elem.icon;
               const isActive = selectedSound === elem.id;
               return (
-                <Button
-                  key={elem.id}
-                  className={`relative rounded-full px-7 py-4 transition-all ${
-                    isActive
-                      ? "bg-primary/75 text-primary-foreground"
-                      : "hover:border-border hover:bg-foreground/5"
-                  }`}
-                  size={"ambient"}
-                  variant={"ambient"}
-                >
-                  <Icon className="w-5 h-5 sm:mr-2" />
-                  <h4 className="text-sm font-medium">{elem.label}</h4>
-                </Button>
+                <motion.div key={elem.id} whileTap={{ scale: 0.95 }}>
+                  <Button
+                    className={`relative rounded-full px-7 py-4 transition-all ${
+                      isActive
+                        ? "bg-primary/75 text-primary-foreground"
+                        : "hover:border-border hover:bg-foreground/5"
+                    }`}
+                    size={"ambient"}
+                    variant={"ambient"}
+                    onClick={() => ToggleEnvironment(elem.id)}
+                  >
+                    <Icon className="w-5 h-5 sm:mr-2" />
+                    <h4 className="text-sm font-medium">{elem.label}</h4>
+                  </Button>
+                </motion.div>
               );
             })}
           </div>
