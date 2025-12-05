410cd0218846c77450d715d5ca9872fa335e6c3d
Marmo77 <matyskamela@gmail.com>
2025-12-05 19:12:21 +0100

    selecting, muting / unmuting

--- src/components/MainApp/AmbientSounds.tsx
+++ src/components/MainApp/AmbientSounds.tsx
@@ -1,7 +1,17 @@
-import { CloudRainWind, Coffee, Moon, Trees, Waves } from "lucide-react";
-import React, { useState } from "react";
+import {
+  CloudRainWind,
+  Coffee,
+  Moon,
+  Trees,
+  Volume2,
+  VolumeX,
+  Waves,
+} from "lucide-react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { motion } from "motion/react";
+import { Slider } from "../ui/slider";
+import { toast } from "sonner";
 
 type Environment = "rain" | "forest" | "night" | "cafe" | "ocean" | null;
 
@@ -13,6 +23,8 @@ type AmbientSoundsElementsProps = {
 };
 function AmbientSounds() {
   const [selectedSound, setSelectedSound] = useState<Environment>(null);
+  const [isMuted, setIsMuted] = useState(true);
+  const [volume, setVolume] = useState<number[]>([50]);
 
   const AmbietSoundsElements: AmbientSoundsElementsProps[] = [
     { id: "rain", icon: CloudRainWind, label: "Rain", color: "#7dd3fc" },
@@ -25,14 +37,45 @@ function AmbientSounds() {
   const ToggleEnvironment = (elem: Environment) => {
     if (selectedSound === elem) {
       setSelectedSound(null);
+      setIsMuted(false);
     } else {
       setSelectedSound(elem);
     }
   };
+  const toggleMuted = () => {
+    const newMutedState = !isMuted;
+    setIsMuted(newMutedState);
+    if (newMutedState) {
+      // stop sounds
+    } else if (selectedSound) {
+      // play sound
+    }
+  };
+  const checkMuted = () => {
+    if (!selectedSound) {
+      toast.info("Please select sound", {
+        richColors: true,
+
+        duration: 3000,
+        dismissible: true,
+      });
+    } else if (isMuted) {
+      toast.error("Please unmute!", {
+        richColors: true,
+        duration: 3000,
+        dismissible: true,
+      });
+    }
+  };
+  // check if volume is on/off if off then toast that user needs to select sound
+  // useEffect(() => {
+
+  // }, [volume]);
 
   return (
     <div className="flex flex-col md:fixed bottom-0 left-0 z-10 py-2 mt-12 w-full bg-card/80 border-border/50 border-t border-x-0 border-b-0">
-      <div className="flex mx-auto w-full max-w-6xl justify-between gap-4 px-6 py-4">
+      <div className="flex mx-auto items-center w-full max-w-6xl justify-between gap-4 px-6 py-4">
+        {/* Ambient Sounds */}
         <div className="w-full flex-4 flex gap-4 items-center">
           <h1 className="text-base font-light">Ambient Sounds</h1>
           <div className="flex gap-4 ">
@@ -59,7 +102,35 @@ function AmbientSounds() {
             })}
           </div>
         </div>
-        <div className="w-full flex-1"></div>
+        {/* Volume settings */}
+        <div className="flex items-center gap-3">
+          <div
+            className="hidden md:flex items-center gap-2 w-32"
+            onClick={checkMuted}
+          >
+            <Slider
+              value={volume}
+              onValueChange={setVolume}
+              max={100}
+              step={1}
+              disabled={isMuted || !selectedSound}
+              className="flex-1"
+            />
+          </div>
+          <Button
+            variant="ambient"
+            size="icon"
+            onClick={toggleMuted}
+            disabled={!selectedSound}
+            className="rounded-full"
+          >
+            {isMuted || !selectedSound ? (
+              <VolumeX className="h-5 w-5" />
+            ) : (
+              <Volume2 className="h-5 w-5" />
+            )}
+          </Button>
+        </div>
       </div>
     </div>
   );
